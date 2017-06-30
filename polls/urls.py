cbca92d35fa7d036ed5577a4489ee3e29c8a6afc
colonelrascals <pdhildreth@gmail.com>
2017-06-30 12:09:52 -0500

    lol

--- polls/urls.py
+++ polls/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'(?P<question_id>[0-9]+)/results/$',views.results, name='results'),
     url(r'(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
 ]
